c252da279bc6454714297d8f0a8f53349bd9b22d
Dylan <dylanc176@nycstudents.net>
2023-12-15 09:15:09 -0500

    changed line 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi :3");
-        System.out.println("I hate dylan cha");
+        System.out.println("I hate nikita");
         // pusg
         System.out.println("i hate this guy");
         System.out.println("Dylan chan more like dylan gonna be thirty and livinbg in a van");
